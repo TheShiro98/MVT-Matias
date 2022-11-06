b4bd5e4a8c494bc77f331e819853bd03ceb29794
Matias Shiroma <cap.shiro@gmail.com>
2022-11-06 04:20:33 -0300

    MVT terminado

--- Proyecto/App/views.py
+++ Proyecto/App/views.py
@@ -6,5 +6,7 @@ def mostrar_familia(request):
 
     f1 = Familias(nombre = 'Leo', apellido = 'Messi', edad = 23, nacimiento = '1999-10-27')
     f2 = Familias(nombre = 'Dibu', apellido = 'Martinez', edad = 25, nacimiento = '1997-09-03')
+    f3 = Familias(nombre = 'Lautaro', apellido = 'Martinez', edad = 21, nacimiento = '2002-06-08')
 
-    return render(request,'familia.html',{'fam':[f1,f2]})
+    return render(request,'familia.html',{'fam':[f1,f2,f3]})
+
